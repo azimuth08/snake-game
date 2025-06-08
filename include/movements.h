3aca01060064e8cd430a800318e3364b6876038a
Dylan DeBriere <dbd249@miami.edu>
2025-06-08 11:49:50 -0400

    added key movements enum for wasd and arrow keys

---
+++ include/movements.h
@@ -0,0 +1,21 @@
+#ifndef MOVEMENTS_H_
+#define MOVEMENTS_H_
+
+enum 
+{
+    KEY_PRESS_W,
+    KEY_PRESS_A,
+    KEY_PRESS_S,
+    KEY_PRESS_D
+};
+
+enum
+{
+    KEY_PRESS_UP,
+    KEY_PRESS_LEFT,
+    KEY_PRESS_DOWN,
+    KEY_PRESS_RIGHT
+};
+
+
+#endif
